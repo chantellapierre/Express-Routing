76066dac4e05f0b7a7e815cec2b33891c33af7a8
chantellapierre <66752117+chantellapierre@users.noreply.github.com>
2021-06-06 14:51:14 -0700

    Error handler

---
+++ expressError.js
@@ -0,0 +1,12 @@
+/** Adds status to normal JS  error **/
+
+class ExpressError extends Error {
+	constructor(message, status) {
+		super();
+		this.message = message;
+		this.status = status;
+		console.error(this.stack);
+	}
+}
+
+module.exports = ExpressError;
